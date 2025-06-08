c25558c7b03d16c6442f242704dde1a043d4d23d
Kiriox <66013753+Kiriox94@users.noreply.github.com>
2025-06-08 12:36:30 +0200

    Fix libnxtc

--- packages/l/libnxtc/xmake.lua
+++ packages/l/libnxtc/xmake.lua
@@ -1,14 +1,16 @@
 package("libnxtc")
-    set_base("switch-pkg")
-    set_kind("library")
+    set_homepage("https://github.com/DarkMatterCore/libnxtc")
+    set_description("Provides an easy way for libnx-based homebrew applications for the Nintendo Switch to populate a title cache and store it on the console's SD card. ")
+    set_license("ISC")
 
-    on_load(function(package)
-        package:add("deps", "libnx")
-        package:data_set("pkgname", "libnxtc")
-    
-        package:base():script("load")(package)
-    end)
-    
-    on_install(function(package)
-        package:base():script("install")(package)
+    set_urls("https://github.com/DarkMatterCore/libnxtc/archive/refs/tags/v$(version).tar.gz")
+    --insert version
+    add_versions("0.0.2", "e3e9bfe51c6ca25d5baccbd85a058d598f6a4609ccd10c70ae374b3d191fde8e")
+
+    add_deps("libnx")
+
+    on_install(function (package)
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
     end)
